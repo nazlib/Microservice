88fecc9ae10eb8b0d97fc3872ab6801c580d6f97
nazlib <nazlibalatlioglu@gmail.com>
2022-11-15 15:25:21 +0300

    photosotock2

--- Services/Basket/FreeCourse.Services.Basket/Dtos/BasketDto.cs
+++ Services/Basket/FreeCourse.Services.Basket/Dtos/BasketDto.cs
@@ -1,10 +1,19 @@
-﻿using System;
+﻿
+using System;
 namespace FreeCourse.Services.Basket.Dtos
 {
     public class BasketDto
     {
-        public BasketDto()
+        public string UserId { get; set; }
+
+        public string DiscountCode { get; set; }
+
+        public int? DiscountRate { get; set; }
+        public List<BasketItemDto> basketItems { get; set; }
+
+        public decimal TotalPrice
         {
+            get => basketItems.Sum(x => x.Price * x.Quantity);
         }
     }
 }
--- Services/Basket/FreeCourse.Services.Basket/Dtos/BasketItemDto.cs
+++ Services/Basket/FreeCourse.Services.Basket/Dtos/BasketItemDto.cs
@@ -3,9 +3,12 @@ namespace FreeCourse.Services.Basket.Dtos
 {
     public class BasketItemDto
     {
-        public BasketItemDto()
-        {
-        }
+        public int Quantity { get; set; }
+
+        public string CourseId { get; set; }
+        public string CourseName { get; set; }
+
+        public decimal Price { get; set; }
     }
 }
 
