2c767401597540eb306bc71ed3ada32bc93a1c2c
nazlib <nazlibalatlioglu@gmail.com>
2022-12-14 09:40:29 +0300

    basket servis added

---
+++ Services/Basket/FreeCourse.Services.Basket/Controllers/BasketsController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FreeCourse.Services.Basket.Dtos;
+using FreeCourse.Services.Basket.Services;
+using FreeCourse.Shared.BaseController;
+using FreeCourse.Shared.Services;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace FreeCourse.Services.Basket.Controllers
+{
+    [Route("api/[controller]")]
+    public class BasketsController : CustomBaseController
+    {
+        private readonly IBasketService _basketService;
+        private readonly ISharedIdentityService _sharedIdentityService;
+
+        public BasketsController(IBasketService basketService, ISharedIdentityService sharedIdentityService)
+        {
+            _basketService = basketService;
+            _sharedIdentityService = sharedIdentityService;
+        }
+        // GET: api/values
+        [HttpGet]
+        public async Task<IActionResult> GetBasket()
+        {
+            return CreateActionResultInstance(await _basketService.GetBasket(_sharedIdentityService.GetUserId));
+        }
+
+       
+        // POST api/values
+        [HttpPost]
+        public async Task<IActionResult> SaveOrUpdateBasket(BasketDto basketDto)
+        {
+            var response = await _basketService.SaveOrUpdate(basketDto);
+            return CreateActionResultInstance(response);
+        }
+
+        // DELETE api/values/5
+        [HttpDelete]
+        public async Task<IActionResult> DeleteBasket()
+        {
+            return CreateActionResultInstance(await _basketService.Delete(_sharedIdentityService.GetUserId));
+        }
+    }
+}
+
--- Services/Basket/FreeCourse.Services.Basket/Program.cs
+++ Services/Basket/FreeCourse.Services.Basket/Program.cs
@@ -1,7 +1,22 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using FreeCourse.Services.Basket.Services;
+using FreeCourse.Services.Basket.Settings;
+using FreeCourse.Shared.Services;
+using Microsoft.Extensions.Options;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddScoped<ISharedIdentityService, SharedIdentityService>();
+builder.Services.AddScoped<IBasketService, BasketService>();
+builder.Services.Configure<RedisSettings>(builder.Configuration.GetSection("RedisSettings"));
+builder.Services.AddSingleton<RedisService>(sp =>
+{
+    var redisSettings = sp.GetRequiredService<IOptions<RedisSettings>>().Value;
+    var redis = new RedisService(redisSettings.Host, redisSettings.Port);
+    redis.Connect();
+    return redis;
+});
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
