26a59b3ac908d5a85b9064195f39bf1350efc86b
nazlib <nazlibalatlioglu@gmail.com>
2023-01-18 23:43:34 +0300

    frontend

---
+++ Frontends/FreeCourse.Web/Services/Interfaces/IIdentityService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using FreeCourse.Shared.Dtos;
+using FreeCourse.Web.Models;
+using IdentityModel.Client;
+
+namespace FreeCourse.Web.Services.Interfaces
+{
+	public interface IIdentityService
+    {
+        Task<Response<bool>> SignIn(SigninInput signinInput);
+
+        Task<TokenResponse> GetAccessTokenByRefreshToken();
+
+        Task RevokeRefreshToken();
+    }
+}
+
